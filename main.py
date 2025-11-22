7981e26b938f5e7214c66f986f298834e030554a
VilMe <vilma.medrano00@gmail.com>
2025-11-21 21:12:15 -0600

    define index phrasses for output

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from flask import Flask, request
 from random import randint, choice
 from datetime import datetime
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    phrases: list[str] = choice(['Welcome to this page!', 'Goose is a lovely boy dog!!', 'It\'s a wonderful day for a dog walk', 'You are looking great today!', 'Goose is looking beautful today!', 'The weather is excellent today!'])
