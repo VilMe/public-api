adc3d127a4d5610a7db2fb90099e5c437ad82b77
VilMe <vilma.medrano00@gmail.com>
2025-11-22 22:05:18 -0600

    add more phrases to list of phrases

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    phrases: list[str] = ['Welcome to this page!', 'Goose is a lovely boy dog!!', 'It\'s a wonderful day for a dog walk', 'You are looking great today!', 'Goose is looking beautful today!', 'The weather is excellent today!']
+    phrases: list[str] = ['Welcome to this page!', 'Goose is a lovely boy dog!!', 'It\'s a wonderful day for a dog walk', 'You are looking great today!', 'Goose is looking beautful today!', 'The weather is excellent today!', 'You are looking wonderful today', 'What an excellent time for a walk with my favorite dog!!']
     return {'phrase': choice(phrases),
             'date': datetime.now()}
+
+
 
 
 
